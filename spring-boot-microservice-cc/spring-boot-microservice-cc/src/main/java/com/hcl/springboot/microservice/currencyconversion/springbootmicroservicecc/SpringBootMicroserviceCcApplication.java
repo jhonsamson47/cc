a3b8fe8c118f73dac41347a8d84cb97981fb14de
jhonsamson47 <jhon.samson47@gmail.com>
2018-06-08 06:11:36 +0530

    Initial commit

---
+++ spring-boot-microservice-cc/spring-boot-microservice-cc/src/main/java/com/hcl/springboot/microservice/currencyconversion/springbootmicroservicecc/SpringBootMicroserviceCcApplication.java
@@ -0,0 +1,12 @@
+package com.hcl.springboot.microservice.currencyconversion.springbootmicroservicecc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCcApplication.class, args);
+	}
+}
